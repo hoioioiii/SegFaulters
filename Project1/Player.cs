6110f6a996ecc105a0afac203f60fd7d9a6c4ee2
Jack <jackdd64@gmail.com>
2023-09-16 19:08:09 -0400

    Added some intro keyboard code.

--- Project1/Player.cs
+++ Project1/Player.cs
@@ -1,9 +1,32 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 public class Player
 {
-	public Player()
+    private KeyboardController KeyboardCtrlr;
+
+    public Player()
 	{
 		// this is the player controller
+
+		void PlayerControllerInitialize()
+        {
+            KeyboardCtrlr = new KeyboardController();
+        }
+        
+        void PlayerControllerUpdate()
+		{           
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // keyboard inputs
+            var kState = Keyboard.GetState();
+
+            if (kState.IsKeyDown(Keys.D0)) { Exit(); }
+            else if (kState.IsKeyDown(Keys.D1)) { option = 1; }
+            else if (kState.IsKeyDown(Keys.D2)) { option = 2; }
+            else if (kState.IsKeyDown(Keys.D3)) { option = 3; }
+            else if (kState.IsKeyDown(Keys.D4)) { option = 4; }
+        }
 	}
 }
