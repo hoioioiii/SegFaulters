9d37bc122c255da2749eeadda9334c6e6cd009aa
Jashira Herrera Brito <herrerabrito.1@buckeyemail.osu.edu>
2023-09-23 15:37:14 -0400

    Project before solution

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Project1.HealthSystem;
 
 namespace Project1
 {
---
+++ Project1/HealthSystem.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HealthSystem
+{
+    public float healthCurrent;
+    public float healthMax;
+
+    public void HealthSystemInstantiate()
+	{
+        this.healthCurrent = healthMax;
+        healthCurrent = healthMax;
+
+	}
+
+    public float HealthCurrent
+    {
+        return healthCurrent;
+    }
+
+
+    public void HealthDamage()
+    {
+
+    }
+
+    public void HealthHeal()
+    {
+
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        // Add any update logic here, if needed
+    }
+
+    public void Draw(SpriteBatch spriteBatch, Vector2 position)
+    {
+        // Add drawing logic here, if needed
+    }
+
+}
