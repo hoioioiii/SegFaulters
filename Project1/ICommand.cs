2279cce996d73b2a3a2cbbd4013940a2a5e9bc6a
drishtiMitt <144497718+drishtiMitt@users.noreply.github.com>
2023-10-01 03:47:06 -0400

    Add files via upload

---
+++ Project1/ICommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+
+    /*
+     * This is to test the Icommands
+     */
+    internal interface ICommand
+    {
+        //interface methods
+        void Execute();
+
+    }
+}
---
+++ Project1/LinkSprite.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using static System.Net.Mime.MediaTypeNames;
+using static Project1.Constants;
+
+namespace Project1
+{
+    public class LinkSprite : IPlayerSprite
+    {
+
+        //scaling factor
+        private static int spriteScale = 4;
+
+        //tells us which directio it is facing
+        public static int linkDirection = 2;
+
+        private static Texture2D[] stillFrames = new Texture2D[PLAYER_FRAMES];
+        private static Texture2D[] movingFrames = new Texture2D[PLAYER_FRAMES];
+        private static Texture2D[] attackFrames = new Texture2D[PLAYER_FRAMES];
+
+        private Texture2D[] currentFrames = new Texture2D[PLAYER_FRAMES];
+
+        //store position
+        private static Vector2 position;
+
+        public LinkSprite(Texture2D[] one, Texture2D[] two, Texture2D[] attack)
+        {
+            stillFrames = one;
+            movingFrames = two;
+            attackFrames = attack;
+
+        }
+
+        public void Update(int direction, Vector2 pos)
+        {
+            linkDirection = direction;
+            position = pos;
+          
+        }
+
+        private void setCurrentFrames(string type)
+        {
+            switch (type)
+            {
+                case "attack":
+                    currentFrames = attackFrames;
+                    break;
+                case "still":
+                    currentFrames = stillFrames;
+                    break;
+                case "move":
+                    currentFrames = movingFrames;
+                    break;
+                default:
+                    currentFrames = stillFrames;
+                    break;
+
+            }
+        }
+
+        private void DrawLink(SpriteBatch sprBatch, int direction)
+        {
+            Rectangle DEST_REC;
+            Rectangle SOURCE_REC;
+            DEST_REC = new Rectangle((int)position.X, (int)position.Y, currentFrames[direction].Width * spriteScale, currentFrames[direction].Height * spriteScale);
+            SOURCE_REC = new Rectangle(0, 0, currentFrames[direction].Width, currentFrames[direction].Height);
+            sprBatch.Draw(currentFrames[direction], DEST_REC, SOURCE_REC, Color.White);
+        }
+
+        public void Draw(SpriteBatch spriteBatch, string _type)
+        {
+            setCurrentFrames(_type);
+            switch (linkDirection)
+            {
+               case 1:
+                    
+                    DrawLink(spriteBatch, (int)DIRECTION.up);
+                    break;
+               case 2:
+         
+                    DrawLink(spriteBatch, (int)DIRECTION.right);
+                    break;
+               case 3:
+                    
+                    DrawLink(spriteBatch, (int)DIRECTION.down);
+                    break;
+               case 4:
+                    
+                    DrawLink(spriteBatch, (int)DIRECTION.left);
+                    break;
+               default:
+                    
+                    DrawLink(spriteBatch, (int)DIRECTION.right);
+                    break;
+            }
+            
+        }
+    }
+}
