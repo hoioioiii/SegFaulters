5bd168083020202b05250f7896322bd85344d434
drishtiMitt <144497718+drishtiMitt@users.noreply.github.com>
2023-10-01 00:39:20 -0400

    Create ItemIterateForwardCommand.cs

---
+++ Project1/ItemIterateForwardCommand.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class EntityIterateForwardCommand : ICommand
+    {
+        public void Execute()
+        {
+            Game1.Item = ItemIterator.getCurrEnemy(false);
+        }
+    }
+}
