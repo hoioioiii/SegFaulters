c543ce2299f6263c43c4f8c769e4ffea946273ad
Kyler McElroy <relyk10@gmail.com>
2023-10-08 14:48:10 -0400

    Instantiation of grid system class, and basic frame work of how the class will work.

---
+++ Project1/GridSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    //Provides grid system details using the methods
+    public static class GridSystem
+    {
+        //static variables
+        //<(int row, int col), int pos>
+        private static Dictionary<(int, int), int> pixelDictionary;
+        private static int rowCount;
+        private static int colCount;
+
+        static GridSystem()
+        {
+            for (int row = 1; row <= rowCount; row++)
+            {
+                for(int col = 1; col <= colCount; col++)
+                {
+                    // assign pixel positions to pixelDictionary
+                }
+            }
+        }
+       
+        
+    }
+}
