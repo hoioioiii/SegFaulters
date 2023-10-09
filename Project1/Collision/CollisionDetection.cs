651c8dffee7befe4a6da39fbacf15ff8eed3b639
Jack <jackdd64@gmail.com>
2023-10-09 17:13:38 -0400

    Made an outline for the collision code to implement

--- Project1/Collision/CollisionDetection.cs
+++ Project1/Collision/CollisionDetection.cs
@@ -1,26 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Content;
+﻿using Microsoft.Xna.Framework;
 
 namespace Project1.Collision
 {
     internal class CollisionDetection
     {
-        public Rectangle rect;
-        
-        public void Update(GameTime gameTime) 
-        { 
-            
+        /*
+         * Detect every object the player could be colliding with
+         * For each player, detect if colliding with list of colliders
+         * Walls, doors, enemies, items, etc.
+         * Knock player back if collision is a wall or enemy (handled in collision response class)
+         * Additional optimizations needed
+         */
+        Rectangle playerRect;
+
+        // List of rectangles for collision boxes
+        Rectangle rect;
+
+        public void Update(GameTime gameTime)
+        {
+
         }
 
         #region Collision Detection
+        /*
+         * Detect collision
+         * if collision is true and object is a wall or enemy, call DetectCollisionDirection()
+         */
+        bool DetectCollision()
+        {
+            bool isColliding = false;
+
+            isColliding = playerRect.Intersects(rect);
+
+            return isColliding;
+        }
+
+        /*
+         * If a rectangle intersection is detected 
+         * and if collision is true and object is a wall or enemy,
+         * 
+         */
+        int DetectCollisionDirection()
+        {
+            // The directions are:
+            int direction = 0;
+
+            return direction;
+        }
+        #endregion
+
+        #region Rectangle intersect test
+
+        #endregion
+
+        #region More Accurate Rectangle intersect test
+        // Rectangle intersect test
         bool IsTouchingLeft()
         {
             bool isTouchingLeft = false;
