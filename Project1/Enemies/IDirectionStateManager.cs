160faf61409c6aa55d0c649d916c71cb0e499826
sesaMeeso <tinalin036@gmail.com>
2023-10-18 01:16:39 -0400

    Added Direction Manger

---
+++ Project1/Enemies/IDirectionStateManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static Project1.Constants;
+
+namespace Project1.Enemies
+{
+    public interface IDirectionStateManager
+    {
+        public Direction getDirection();
+        public void setDirection(Direction direction);
+
+        public void changeDirection();
+
+        public void NeedDirectionUpdate(bool dir_flag);
+    }
+}
