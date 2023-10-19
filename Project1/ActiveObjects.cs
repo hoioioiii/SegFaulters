1ccc5dfd71331402c80008b8edd1099c202456c0
Tina-help <tinalin036@gmail.com>
2023-10-19 15:36:16 -0400

    Added Update and Draw

--- Project1/ActiveObjects.cs
+++ Project1/ActiveObjects.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Project1
 {
@@ -12,12 +13,19 @@ namespace Project1
         private List<IItem>items;
         private List<IEntity> entities;
         private List<IWeapon> weapons;
+        private Player link;
+        
 
         public ActiveObjects() {
-
             items = new List<IItem>();
             entities = new List<IEntity>();
             weapons = new List<IWeapon>();
+            
+        }
+
+        public void addLink(Player link)
+        {
+            this.link = link;
         }
 
         public void addNewEntity(IEntity entity)
@@ -42,6 +50,7 @@ namespace Project1
             weapons.Clear();
         }
 
+        public Player getLink() { return link; }
         public List<IEntity> getEntityList()
         {
             return entities;
@@ -70,6 +79,91 @@ namespace Project1
         public void removeWeapon(IWeapon weapon)
         {
             weapons.Remove(weapon);
+        }
+
+        public void Update()
+        {
+
+            UpdateItems();
+
+            UpdateEnemies();
+
+            UpdateIWeapons();
+
+            UpdatePlayer();
+
+
+
+        }
+
+        private void UpdatePlayer()
+        {
+            //link.Update();
+        }
+
+        private void UpdateIWeapons()
+        {
+            for (int i = 0; i < weapons.Count; i++)
+            {
+                weapons[i].Update();
+            }
+        }
+
+        private void UpdateEnemies()
+        {
+            for (int i = 0; i < entities.Count; i++)
+            {
+                entities[i].Update();
+            }
+        }
+
+        private void UpdateItems()
+        {
+            for (int i = 0; i < entities.Count; i++)
+            {
+                items[i].Update();
+            }
+        }
+
+
+        private void DrawPlayer()
+        {
+          
+                //TODO:Fix later
+               // link.Draw(, Game1._spriteBatch);
+            
+        }
+
+        private void DrawIWeapons()
+        {
+            for (int i = 0; i < weapons.Count; i++)
+            {
+                weapons[i].Draw();
+            }
+        }
+
+        private void DrawEnemies()
+        {
+            for (int i = 0; i < entities.Count; i++)
+            {
+                entities[i].Draw(Game1._spriteBatch);
+            }
+        }
+
+        private void DrawItems()
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                items[i].Draw(Game1._spriteBatch);
+            }
+        }
+
+        public void Draw()
+        {
+            DrawPlayer();
+            DrawIWeapons();
+            DrawEnemies();
+            DrawItems();
         }
     }
 }
