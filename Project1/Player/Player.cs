1b02f0c9e685fa3d770d4794beb865dd7f777ab8
drishtiMitt <144497718+drishtiMitt@users.noreply.github.com>
2023-11-01 02:43:09 -0400

    Cleaned up Player.cs file but there is some issues with the sword

--- Project1/Player/Player.cs
+++ Project1/Player/Player.cs
@@ -32,26 +32,6 @@ namespace Project1
         // cardinal direction player is facing, starts with up on 1 and progresses clockwise (e.g. 4 is left-facing)
         private static int linkDirection = (int)DIRECTION.right;
 
-        /*
-        // frames used for still and animation
-        public static Texture2D linkRight1;
-        public static Texture2D linkLeft1;
-        public static Texture2D linkUp1;
-        public static Texture2D linkDown1;
-
-        // frames used for animation only
-        public static Texture2D linkRight2;
-        public static Texture2D linkLeft2;
-        public static Texture2D linkUp2;
-        public static Texture2D linkDown2;
-
-        // frames used for attacking
-        public static Texture2D linkAttackRight;
-        public static Texture2D linkAttackLeft;
-        public static Texture2D linkAttackUp;
-        public static Texture2D linkAttackDown;
-        */
-
         // attacking metrics
         public static bool isAttacking = false;
         private static float AttackTimer;
@@ -78,22 +58,16 @@ namespace Project1
 
         private static IWeapon[] weaponsArray;
         //private static IWeapon weapon;
-        private static int currentWeaponIndex;
         private static IWeapon spriteWeapon;
-        private static IActiveObjects objManager;
 
         private static int onScreen;
 
-        //private static int posX;
-        //private static int posY;
-        //private Rectangle rect;
+
         private static bool remainOnScreen;
         private static int damageFlash;
         public Player()
         {
 
-            //posX = 0;
-            //posY = 0;
             remainOnScreen = false;
         }
 
@@ -105,18 +79,9 @@ namespace Project1
             DamageTimer = Constants.INVINCIBILITY_SECONDS;
             FlashTimer = Constants.FLASHTIME;
             damageFlash = 0;
-            //IWeapon[] temp = { new Bomb(), new Arrow2(), new Boomerange()};
-            //weaponsArray = temp;
-            //weaponsArray[0] = new Bomb();
-            //weaponsArray[1] = new Arrow2();
-            //weaponsArray[2] = new Boomerange();
-            //weaponsArray[3] = new Orb();
-
-            objManager = new ActiveObjects();
 
             sprite = PlayerSpriteFactory.Instance.CreateLinkSprite();
 
-            // weapon = new Bomb();
 
             BoundingBox = new Rectangle(0, 0, LINK_BOUNDING_DIMENSION, LINK_BOUNDING_DIMENSION);
         }
@@ -130,25 +95,6 @@ namespace Project1
             IWeapon[] temp = { new Bomb(), new Arrow2(), new Boomerange(), new Sword() };
             weaponsArray = temp;
 
-            /*
-            // still and movement
-            linkRight1 = content.Load<Texture2D>("linkRight1");
-            linkLeft1 = content.Load<Texture2D>("linkLeft1");
-            linkUp1 = content.Load<Texture2D>("linkUp1");
-            linkDown1 = content.Load<Texture2D>("linkDown1");
-
-            // movement only
-            linkRight2 = content.Load<Texture2D>("linkRight2");
-            linkLeft2 = content.Load<Texture2D>("linkLeft2");
-            linkUp2 = content.Load<Texture2D>("linkUp2");
-            linkDown2 = content.Load<Texture2D>("linkDown2");
-
-            // Attack using weapon or item
-            linkAttackRight = content.Load<Texture2D>("linkAttackRight");
-            linkAttackLeft = content.Load<Texture2D>("linkAttackLeft");
-            linkAttackUp = content.Load<Texture2D>("linkAttackUp");
-            linkAttackDown = content.Load<Texture2D>("linkAttackDown");
-            */
         }
 
 
@@ -164,7 +110,6 @@ namespace Project1
             // set bounding box position to link position
             BoundingBox.Location = new Point((int)position.X + 5, (int)position.Y + 20);
             // move link to bounding box
-            //sprite.Update(linkDirection, position);
             // call collision and pass in link
 
             if (isDamaged)
@@ -236,58 +181,8 @@ namespace Project1
 
                 }
 
-
-                /*
-
-                if (keystate.IsKeyDown(Keys.Left) || keystate.IsKeyDown(Keys.A))
-                {
-                    position.X -= playerSpeed;
-                    //room boundary controller, roomBounds var may need to be changed to new values
-                    position.X = Math.Clamp(position.X, roomBoundsMinX, roomBoundsMaxX);
-                    isMoving = true;
-                    linkDirection = (int)DIRECTION.left;
-
-                    //posX -= playerSpeed;
-
-
-                }
-                else if (keystate.IsKeyDown(Keys.Down) || keystate.IsKeyDown(Keys.S))
-                {
-                    position.Y += playerSpeed;
-                    position.Y = Math.Clamp(position.Y, roomBoundsMinY, roomBoundsMaxY);
-                    isMoving = true;
-                    linkDirection = (int)DIRECTION.down;
-                    //posX += playerSpeed;
-
-                }
-                else if (keystate.IsKeyDown(Keys.Up) || keystate.IsKeyDown(Keys.W))
-                {
-                    position.Y -= playerSpeed;
-                    position.Y = Math.Clamp(position.Y, roomBoundsMinY, roomBoundsMaxY);
-                    isMoving = true;
-                    linkDirection = (int)DIRECTION.up;
-                    //posY -= playerSpeed;
-                }
-                else if (keystate.IsKeyDown(Keys.Right) || keystate.IsKeyDown(Keys.D))
-                {
-                    position.X += playerSpeed;
-                    position.X = Math.Clamp(position.X, roomBoundsMinX, roomBoundsMaxX);
-                    isMoving = true;
-                    linkDirection = (int)DIRECTION.right;
-                    //posY += playerSpeed;
-                }
-                */
-
-                //move method to increment/decrement position depending on direction
             }
 
-            /*
-            if (keystate.IsKeyDown(Keys.E))
-            {
-                DamageInvincibility();
-            }
-            */
-            //Move(keystate);
             //Is not always returning to display state at end
             if (keystate.IsKeyDown(Keys.E))
             {
@@ -310,10 +205,10 @@ namespace Project1
 
 
 
-            //if (remainOnScreen)
-            //{
-            //    spriteWeapon.Update();
-            //}
+            if (remainOnScreen)
+            {
+                spriteWeapon.Update();
+            }
 
         }
 
@@ -343,7 +238,7 @@ namespace Project1
             float elapsedSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
             FrameTimer -= elapsedSeconds;
 
-            //CheckOnScreen();
+            CheckOnScreen();
 
             //manage non persistent weapon
             if (remainOnScreen)
@@ -360,7 +255,7 @@ namespace Project1
             {
                 if (isAttacking)
                 {
-                    //DrawBasedOnAttackType(spriteBatch);
+                    DrawBasedOnAttackType(spriteBatch);
 
                     //if (isAttackingWithSword)
                     //{
@@ -424,36 +319,7 @@ namespace Project1
                 }
                 else
                 {
-
                     DrawBasedOnMovementType(spriteBatch);
-                    /*
-                    if (isMoving)
-                    {
-                        //can be condensed since it should default to still
-                        //when its is not on secondFrame && isMoving
-
-                        DrawBasedOnMove();
-                        CheckFrameTimer();
-                        if (isSecondFrame)
-                        {
-                            //tell sprite how to draw
-                            sprite.Draw(spriteBatch, "move", linkDirection, position);
-
-                        }
-                        else
-                        {
-                            //tell sprite how to draw
-                            sprite.Draw(spriteBatch, "still", linkDirection, position);
-                        }
-
-                    }
-                    else
-                    {
-                        //tell sprite how to draw
-                        sprite.Draw(spriteBatch, "still", linkDirection, position);
-
-                    }
-                    */
                 }
             }
             
@@ -502,21 +368,21 @@ namespace Project1
             {
                 //add weapon to manager so it'll stay on the screen
                 spriteWeapon = new Boomerange();
-                objManager.addNewWeapon(spriteWeapon);
+                Game1.GameObjManager.addNewWeapon(spriteWeapon);
 
             }
             else if (isAttackingWithBow)
             {
                 // add weapon to manager so it'll stay on the screen
                 spriteWeapon = new Arrow2();
-                objManager.addNewWeapon(spriteWeapon);
+                Game1.GameObjManager.addNewWeapon(spriteWeapon);
 
             }
             else if (isAttackingWithBomb)
             {
                 // add weapon to manager so it'll stay on the screen
                 spriteWeapon = new Bomb();
-                objManager.addNewWeapon(spriteWeapon);
+                Game1.GameObjManager.addNewWeapon(spriteWeapon);
 
             }
             //draw link with attack frames
@@ -524,47 +390,18 @@ namespace Project1
         }
 
 
-        //Draw a weapon that does not need to be persistent on screen
+        //Draw a weapon that does not need to be persistent on screen (Mostly sword)
         public static void DrawNonpersistantWeapon(int weaponType)
         {
-            //set spriteWeapon to element in weapon array to get rid of these
-            //if branches
 
-            //weapon = weaponsArray[weaponType];
+            spriteWeapon = weaponsArray[weaponType];
 
+            remainOnScreen = true;
+            spriteWeapon.Attack();
+            spriteWeapon.Draw();
 
-            //spriteWeapon = weaponsArray[weaponType];
-            spriteWeapon = new Bomb();
+            //This should be in DrawBasedOnAttackType()
             /*
-            switch (weaponType)
-            {
-                case "bomb":
-                    weapon = new Bomb();
-                    spriteWeapon = weapon;
-                    break;
-
-                case "bow":
-                    weapon = new Arrow2();
-                    spriteWeapon = weapon;
-                    break;
-                //TODO:Reimplement later
-                //case "boomerange":
-                //    weapon = new Boomerange();
-                //    spriteWeapon = weapon;
-                //    break;
-                case "orb":
-                    weapon = new Orb();
-                    spriteWeapon = weapon;
-                    break;
-
-            }
-            */
-
-            //remainOnScreen = true;
-            //spriteWeapon.Attack();
-            //spriteWeapon.Draw();
-
-
             switch (weaponType)
             {
                 case 0:
@@ -578,7 +415,7 @@ namespace Project1
                     //    Game1.GameObjManager.addNewWeapon(new Boomerange());
                     //    break;
             }
-
+            */
 
 
 
@@ -604,22 +441,7 @@ namespace Project1
 
         // Link cannot take damage for x seconds after getting hit
         public static void DamageInvincibility()
-        {
-            //if (DamageTimer <= 0)
-            //{
-            //    renderLink = true;
-            //    isDamaged = false;
-            //    DamageTimer = INVINCIBILITY_SECONDS;
-            //}
-            //else
-            //{
-            //    if (FlashTimer <= 0)
-            //    {
-            //        renderLink = !renderLink;
-            //        FlashTimer = FLASHTIME;
-            //    }
-            //}
-            
+        {      
             if (damageFlash <= 50)
             {
                 CheckTime();
@@ -649,28 +471,24 @@ namespace Project1
         {
             isAttacking = true;
             isAttackingWithSword = true;
-            //currentWeaponIndex = (int)WEAPONS.sword;
         }
 
         public static void attackBoom()
         {
             isAttacking = true;
             isAttackingWithBoomerang = true;
-            //currentWeaponIndex = (int)WEAPONS.boom;
         }
 
         public static void attackBow()
         {
             isAttacking = true;
             isAttackingWithBow = true;
-            //currentWeaponIndex = (int)WEAPONS.bow;
         }
 
         public static void attackBomb()
         {
             isAttacking = true;
             isAttackingWithBomb = true;
-            //currentWeaponIndex = (int)WEAPONS.bomb;
         }
 
         public static void left()
