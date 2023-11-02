bec5db316548498fc3697507bae6fced3434bba7
drishtiMitt <144497718+drishtiMitt@users.noreply.github.com>
2023-11-02 05:24:28 -0400

    Added intial code

---
+++ Project1/HUD/HeadsUpDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project1.HUD
+{
+	public class HeadsUpDisplay : IHUD
+	{
+		private IHUDEntity location;
+        private IHUDEntity inventory;
+        private IHUDEntity health;
+
+        private static bool isFullMenu;
+
+        public HeadsUpDisplay()
+		{
+            location = new LocationDisplay();
+            inventory = new InventoryDisplay();
+            health = new HealthDisplay();
+        }
+
+        public void Update(bool fullMenu)
+        {
+            isFullMenu = fullMenu;
+            location.Update();
+            inventory.Update();
+            health.Update();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            location.Draw(spriteBatch);
+            inventory.Draw(spriteBatch);
+            health.Draw(spriteBatch);
+        }
+
+        public static bool IsFullMenuDisplay()
+        {
+            return isFullMenu;
+        }
+	}
+}
+
---
+++ Project1/HUD/LocationDisplay.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using static System.Formats.Asn1.AsnWriter;
+using static Project1.Constants;
+using static Project1.Game1;
+using System.Drawing;
+using Color = Microsoft.Xna.Framework.Color;
+
+namespace Project1.HUD
+{
+	public class LocationDisplay : IHUDEntity
+    {
+        private Texture2D titleRect;
+        private Texture2D mapRect;
+        private Vector2 coordTitle;
+        private Vector2 coordMap;
+        private int fullMenuOffset = 0;
+        private bool reset = false;
+
+        public LocationDisplay()
+		{
+            titleRect = new Texture2D(Game.GraphicsDevice, SCREEN_WIDTH_UPPER / 3, SCREEN_WIDTH_UPPER / 10);
+            mapRect = new Texture2D(Game.GraphicsDevice, SCREEN_WIDTH_UPPER / 3, SCREEN_WIDTH_UPPER / 7);
+            coordTitle = new Vector2(0, 0);
+            coordMap = new Vector2(0, titleRect.Height);
+        }
+
+        public void Update()
+        {
+            if(HeadsUpDisplay.IsFullMenuDisplay())
+            {
+                coordTitle.Y += fullMenuOffset;
+                coordMap.Y += fullMenuOffset;
+                reset = true;
+            } else if(reset)
+            {
+                coordTitle.Y -= fullMenuOffset;
+                coordMap.Y -= fullMenuOffset;
+                reset = false;
+            }
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(titleRect, coordTitle, Color.White);
+            spriteBatch.Draw(mapRect, coordMap, Color.Blue);
+        }
+    }
+}
+
