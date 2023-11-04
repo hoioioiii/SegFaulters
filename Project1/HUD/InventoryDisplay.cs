e40e5a2ec089ddcdbc6bffffadc32573263a9736
drishtiMitt <144497718+drishtiMitt@users.noreply.github.com>
2023-11-04 18:48:35 -0400

    Got colored squares working

--- Project1/HUD/InventoryDisplay.cs
+++ Project1/HUD/InventoryDisplay.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework;
 using static System.Formats.Asn1.AsnWriter;
 using static Project1.Constants;
 using static Project1.Game1;
-using System.Drawing;
+//using System.Drawing;
 using Color = Microsoft.Xna.Framework.Color;
 using System.Reflection.Metadata;
 using Microsoft.Xna.Framework.Content;
@@ -15,9 +15,11 @@ namespace Project1.HUD
     {
         private Texture2D countRect;
         private Texture2D itemRect;
+        private Texture2D innerItemRect;
         private Vector2 coordCountBase;
         private Vector2 coordCount;
         private Vector2 coordItem;
+        private Rectangle itemDestination;
         private int fullMenuOffset = 0;
         private int HUD_COUNT_OFFSET = HUD_HEIGHT / 4;
         private int ITEM_SPRITE_OFFSET = HUD_SECTION_WIDTH / 9;
@@ -28,10 +30,17 @@ namespace Project1.HUD
         public InventoryDisplay(GraphicsDevice graphics, ContentManager content)
 		{
             countRect = new Texture2D(graphics, SCREEN_WIDTH / 9, SCREEN_WIDTH / 5);
-            itemRect = new Texture2D(graphics, 2 * (SCREEN_WIDTH / 9), SCREEN_WIDTH / 5);
+            itemRect = new Texture2D(graphics, 1, 1);
+            itemRect.SetData(new[] { Color.Blue });
+
+            innerItemRect = new Texture2D(graphics, 1, 1);
+            innerItemRect.SetData(new[] { Color.Black });
+
+            coordItem = new Vector2(HUD_SECTION_WIDTH + (HUD_SECTION_WIDTH / 3), HUD_HEIGHT / 3);
+            itemDestination = new Rectangle((int)coordItem.X, (int)coordItem.Y, HUD_SECTION_WIDTH / 4, HUD_HEIGHT / 3);
+
             coordCountBase = new Vector2(HUD_SECTION_WIDTH, HUD_COUNT_OFFSET);
             coordCount = coordCountBase;
-            coordItem = new Vector2(HUD_SECTION_WIDTH, SCREEN_WIDTH / 10);
             font = content.Load<SpriteFont>("HUDFont");
 
             //private SpriteFont font = Content.Load<SpriteFont>("File");
@@ -70,13 +79,16 @@ namespace Project1.HUD
             coordCount = coordCountBase;
             rupee.Draw(spriteBatch, coordCount, 2);
 
-            coordCount.Y = coordCountBase.Y + ((HUD_HEIGHT / 3) * 2);
+            //key won't draw right
+            coordCount.Y = coordCountBase.Y + ((HUD_HEIGHT / 3) * 1);
             key.Draw(spriteBatch, coordCount, 2);
 
             coordCount.Y = coordCountBase.Y + ((HUD_HEIGHT / 3) * 2);
             bomb.Draw(spriteBatch, coordCount, 2);
             //spriteBatch.DrawString(font, "-Inventory-", coordCount, Color.White);
-            //spriteBatch.Draw(itemRect, coordItem, Color.Blue);
+            //boxes to hold inventory
+            spriteBatch.Draw(itemRect, itemDestination, Color.Blue);
+            spriteBatch.Draw(itemRect, new Rectangle((int)coordItem.X + (HUD_SECTION_WIDTH / 3), (int)coordItem.Y, HUD_SECTION_WIDTH / 4, HUD_HEIGHT / 3), Color.Blue);
         }
     }
 }
--- Project1/HUD/LocationDisplay.cs
+++ Project1/HUD/LocationDisplay.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework;
 using static System.Formats.Asn1.AsnWriter;
 using static Project1.Constants;
 using static Project1.Game1;
-using System.Drawing;
+//using System.Drawing;
 using Color = Microsoft.Xna.Framework.Color;
 using System.Reflection.Metadata;
 using Microsoft.Xna.Framework.Content;
@@ -26,7 +26,8 @@ namespace Project1.HUD
 		{
             font = content.Load<SpriteFont>("HUDFont");
             titleRect = new Texture2D(graphics, HUD_SECTION_WIDTH, HUD_HEIGHT / 3);
-            mapRect = new Texture2D(graphics, HUD_SECTION_WIDTH, HUD_HEIGHT - titleRect.Height);
+            mapRect = new Texture2D(graphics, 1, 1);
+            mapRect.SetData(new[] { Color.Blue});
             titleOffset = (HUD_SECTION_WIDTH - (int)font.MeasureString("-Location-").X) / 2;
             coordTitle = new Vector2(titleOffset, 0);
             coordMap = new Vector2(0, titleRect.Height);
@@ -56,7 +57,7 @@ namespace Project1.HUD
         {
             //spriteBatch.Draw(titleRect, coordTitle, Color.White);
             spriteBatch.DrawString(font, "-Location-", coordTitle, Color.White);
-            spriteBatch.Draw(mapRect, coordMap, Color.Blue);
+            spriteBatch.Draw(mapRect, new Rectangle((int)coordMap.X, (int)coordMap.Y, HUD_SECTION_WIDTH, HUD_HEIGHT - titleRect.Height), Color.Blue);
         }
     }
 }
