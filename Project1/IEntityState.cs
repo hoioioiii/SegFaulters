5017e038c8996677207aef7dca07504516611983
Tina-help <tinalin036@gmail.com>
2023-11-06 00:52:49 -0500

    Finished adding item drops and enemy death

--- Project1/IEntityState.cs
+++ Project1/IEntityState.cs
@@ -1,4 +1,32 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Project1
+//{
+//    public interface IEntityState
+//    {
+//        public void setIsMoving(bool update);
+//        public bool isMoving();
+//        public void setDamaged(bool update);
+//        public bool isDamaged();
+
+//        public void setIsAttacking(bool update);
+
+//        public bool IsAttacking();
+
+//        public bool IsAlive();
+
+//        public void setIsAlive(bool update);
+//        public void setNewAttack(bool update);
+//        public bool startNewAttack();
+
+//    }
+//}
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +36,9 @@ namespace Project1
 {
     public interface IEntityState
     {
+
+        public bool finished { get; }
+        public void SetFinished();
         public void setIsMoving(bool update);
         public bool isMoving();
         public void setDamaged(bool update);
