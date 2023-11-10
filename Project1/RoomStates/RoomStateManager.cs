142d95b1f51f2086ac806d4ce9e310f4a4868427
fredowill <fredowilliams@proton.me>
2023-11-10 18:48:38 -0500

    got an inital statemanager

---
+++ Project1/RoomStates/RoomStateManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class RoomStateManager
+    {
+        private Dictionary<int, IActiveObjects> roomEntityState; // Map room ID to its active objects
+
+        public RoomStateManager()
+        {
+            roomEntityState = new Dictionary<int, IActiveObjects>();
+        }
+
+        public void SaveCurrentRoomState(IActiveObjects activeObjects)
+        {
+            int currentRoomIndex = RoomManager.GetCurrentRoomIndex();
+            roomEntityState[currentRoomIndex] = activeObjects; // Save the state of active objects for the current room
+        }
+
+        public IActiveObjects LoadCurrentRoomState()
+        {
+            int currentRoomIndex = RoomManager.GetCurrentRoomIndex();
+            if (roomEntityState.ContainsKey(currentRoomIndex))
+            {
+                return roomEntityState[currentRoomIndex]; // Retrieve the state of active objects for the current room
+            }
+            return null; // Handle non-existence of data for the current room
+        }
+    }
+}
