0d622c93052a6c213c8d9072567910017d20514e
Jack <jackdd64@gmail.com>
2023-11-14 23:08:22 -0500

    sped up room transition time from 3 sec to 1.5 sec

--- Project1/Constants.cs
+++ Project1/Constants.cs
@@ -392,7 +392,7 @@ namespace Project1
         }
 
         // Room Transitioning
-        public static float ROOM_TRANSITION_SECONDS = 3;
+        public static float ROOM_TRANSITION_SECONDS = 1.5f;
         public static int TRANSITION_OFFSET_X = 17;
         public static int TRANSITION_OFFSET_Y = -231;
     }
