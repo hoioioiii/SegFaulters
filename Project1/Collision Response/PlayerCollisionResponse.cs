187c375fa81990a32b329d58f144e789010886ce
Jack DeLeo <jackdd64@gmail.com>
2023-11-25 08:49:52 -0500

    added proper item sound effects (heart case does not exist)

--- Project1/Collision Response/PlayerCollisionResponse.cs
+++ Project1/Collision Response/PlayerCollisionResponse.cs
@@ -22,8 +22,7 @@ namespace Project1.Collision_Response
         public static void ItemResponse(IItem item)
         {
             Game1.GameObjManager.removeItem(item);
-            //item.drawState = false;
-            AudioManager.PlaySoundEffect(smallItemGet);
+            //item.drawState = false;        
             Player.PickUpItem(IItemtoITEMS(item));
         }
         //helper method to get player pickupitem method the needed input type
@@ -34,30 +33,39 @@ namespace Project1.Collision_Response
             {
                 case BombItem:
                     ITEMS = ITEMS.Bomb;
+                    AudioManager.PlaySoundEffect(smallItemGet);
                     break;
                 case ArrowItem:
                     ITEMS = ITEMS.Arrow;
+                    AudioManager.PlaySoundEffect(smallItemGet);
                     break;
                 case BoomerangItem:
                     ITEMS = ITEMS.Boomerang;
+                    AudioManager.PlaySoundEffect(smallItemGet);
                     break;
                 case Clock:
                     ITEMS = ITEMS.Clock;
+                    AudioManager.PlaySoundEffect(smallItemGet);
                     break;
                 case Fairy:
                     ITEMS = ITEMS.Fairy;
+                    AudioManager.PlaySoundEffect(smallItemGet);
                     break;
                 case HeartContainer:
                     ITEMS = ITEMS.HeartContainer;
+                    AudioManager.PlaySoundEffect(smallItemGet);
                     break;
                 case Key:
                     ITEMS = ITEMS.Key;
+                    AudioManager.PlaySoundEffect(smallItemGet);
                     break;
                 case Map:
                     ITEMS = ITEMS.Map;
+                    AudioManager.PlaySoundEffect(smallItemGet);
                     break;
                 case Rupee:
                     ITEMS = ITEMS.Rupee;
+                    AudioManager.PlaySoundEffect(rupeeGet);
                     break;
                 case Sword:
                     ITEMS = ITEMS.Sword;
@@ -65,6 +73,8 @@ namespace Project1.Collision_Response
                 case Triforce:
                     ITEMS = ITEMS.Triforce;
                     break;
+                    // Where is the case for heart?
+                    // AudioManager.PlaySoundEffect(heartGet);
 
             }
             return ITEMS;
