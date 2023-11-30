6dafe57139bbffd9ae43eb5d8560ac7e1ba8a6fd
drishtiMitt <144497718+drishtiMitt@users.noreply.github.com>
2023-11-30 17:28:04 -0500

    Found out how to referecne game state and added initial scrolling code. I might’ve gotten the directions wrong

--- Project1/Commands/PauseGame.cs
+++ Project1/Commands/PauseGame.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Project1.HUD;
 using static Project1.Constants;
 
 namespace Project1
@@ -9,12 +10,18 @@ namespace Project1
         {
             Game1.GameStateManager.PauseGame();
 
-            /*
-            if (Game1.GameStateManager.)
+            //check if not paused
+            if (GameStateManager.GameState == GameState.DefaultState)
             {
-
+                //Not sure if I switched the directions
+                HeadsUpDisplay.StartScrolling(DIRECTION.up);
             }
-            */
+            //check if paused
+            else if (GameStateManager.GameState == GameState.PausedState)
+            {
+                HeadsUpDisplay.StartScrolling(DIRECTION.down);
+            }
+           
         }
     }
 }
