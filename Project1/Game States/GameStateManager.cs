e8858ce56fc412ce48e66ece4bf3dd2667d42fef
Kyler McElroy <relyk10@gmail.com>
2023-12-02 11:14:24 -0500

    fixed bug where game restart would mess up the first room

--- Project1/Game States/GameStateManager.cs
+++ Project1/Game States/GameStateManager.cs
@@ -67,7 +67,6 @@ namespace Project1
                     break;
                 case GameState.TriforceWinState:
                     TriforceWinningScreen.Update();
-                    new GameRestart().Execute();
                     break;
                 case GameState.GameOverState:
                     GameOverScreen.Update();
@@ -89,6 +88,7 @@ namespace Project1
         {
             if (Inventory.itemInventory[(int)ITEMS.Triforce] == 1)
             {
+                new GameRestart().Execute();
                 GameState = GameState.TriforceWinState;
                 TriforceWinningScreen.TriForceSoundEffect();
             }
